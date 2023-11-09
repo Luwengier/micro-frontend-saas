191c473a1ff14a2c5af361c80471e46e1a6767ba
Luewngier <g217881111@gmail.com>
2023-11-09 22:51:14 +0800

    Synchronize path from marketing to container

--- packages/container/components/MarketingApp.js
+++ packages/container/components/MarketingApp.js
@@ -1,20 +1,24 @@
-import React, { useEffect, useRef } from 'react'
-import mount from 'marketing/MarketingApp'
+import React, { useEffect, useRef } from 'react';
+import mount from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 const MarketingApp = () => {
-  const ref = useRef(null)
+  const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('Container noticed navigation in Marketing')
-      }
-    })
-  }, [])
+      onNavigate: ({ pathname: nextPathname, search: nextSearch }) => {
+        const { pathname, search } = history.location;
 
-  return (
-    <div ref={ref} />
-  )
-}
+        if (pathname !== nextPathname || search !== nextSearch) {
+          history.push(nextPathname + nextSearch);
+        }
+      },
+    });
+  }, []);
 
-export default MarketingApp
+  return <div ref={ref} />;
+};
+
+export default MarketingApp;
