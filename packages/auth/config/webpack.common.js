17f13fd27d4103601624a9060e25ddaf027b022a
Luewngier <g217881111@gmail.com>
2023-11-12 16:04:36 +0800

    Initialize auth project

---
+++ packages/auth/config/webpack.common.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // only run babel on js files
+        exclude: /node_modules/, // don't run babel on node_modules
+        use: {
+          loader: 'babel-loader', // use babel-loader
+          options: {
+            presets: [ // use these presets (order matters)
+              '@babel/preset-react', // transform jsx to js
+              '@babel/preset-env', // transform modern js to old js
+            ],
+            plugins: [ // use these plugins (order matters)
+              '@babel/plugin-transform-runtime', // async/await
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  onNavigate && history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname, search: nextSearch }) => {
+      if (history.pathname !== nextPathname || history.search !== nextSearch) {
+        history.push(nextPathname + nextSearch);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export default mount;
