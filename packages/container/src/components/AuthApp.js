be88ff20fea9db3eaee5c90045209f2b79373de9
Luewngier <g217881111@gmail.com>
2023-11-16 22:10:10 +0800

    Add auth project to container

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react';
+import mount from 'auth/AuthApp';
+import { useHistory } from 'react-router-dom';
+
+const AuthApp = () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname, search: nextSearch }) => {
+        const { pathname, search } = history.location;
+
+        if (pathname !== nextPathname || search !== nextSearch) {
+          history.push(nextPathname + nextSearch);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, [history]);
+
+  return <div ref={ref} />;
+};
+
+export default AuthApp;
